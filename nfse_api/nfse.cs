40c625b5c1359ec1bf8aeb85faeebc5e76b83212
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2023-05-21 12:17:29 -0300

    iniciado ambiente de desenvolvimento

---
+++ nfse_api/nfse.cs
@@ -0,0 +1,25 @@
+﻿namespace nfse_api
+{
+    public class nfse
+    {
+        public int id { get; set; }
+        public string nome_prestador_servico { get; set; }
+        public string cpf_cnpj_prestador_servico { get; set; }
+        public string inscricao_prestador_servico { get; set; }
+        public string nome_tomador_servico { get; set; }
+        public string cpf_cnpj_tomador_servico { get; set; }
+        public string inscricao_tomador_servico { get; set;}
+        public string numero_nfse { get; set; }
+        public string data_emissao_nfse { get; set; }
+        public string valor_total_nfse { get; set; }
+        public string descricao_servico_prestado { get; set; }
+        public string valor_unitario_servico { get; set; }
+        public string quantidade_unidades_servico { get; set; }
+        public string valor_total_servico { get; set; }
+        public string ISS { get; set; }
+        public string aliquota_iss { get; set; }
+        public string valor_iss_pago { get; set; }
+        public string Observacoes {get; set;}
+
+    }
+}
