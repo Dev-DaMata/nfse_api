8dcf562a99b546e4474376a9e15c60ecd6fb50a3
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2023-05-21 12:47:53 -0300

    adicionado a rota GET para nfse

---
+++ nfse_api/Controllers/NfseController.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace nfse_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NfseController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public NfseController(IConfiguration config) {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<nfse>>> GetAllNfse()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var nfse = await connection.QueryAsync<nfse>("select * from NotaFiscal");
+            return Ok(nfse);
+        }
+    }
+}
